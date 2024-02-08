1c8d15fc5a31540e479b0e45955d9803ee59e575
ulasozturk <ulasozturkdev@gmail.com>
2024-02-08 13:19:37 +0300

    add: sign In and sign Up logic

--- RegisterLesson/Features/Auth/SignIn/SignInVC.swift
+++ RegisterLesson/Features/Auth/SignIn/SignInVC.swift
@@ -1,11 +1,5 @@
-//
-//  SignInVC.swift
-//  RegisterLesson
-//
-//  Created by macbook pro on 7.02.2024.
-//
-
 import UIKit
+import CoreData
 
 class SignInVC: UIViewController {
 
@@ -17,11 +11,49 @@ class SignInVC: UIViewController {
         
         navigationItem.hidesBackButton = true
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: sView!.previousButton)
-        
         sView?.previousButton.addTarget(self, action: #selector(goPrevious), for: .touchUpInside)
+        sView?.loginButton.addTarget(self, action: #selector(signIn), for: .touchUpInside)
     }
     @objc func goPrevious(){
         navigationController?.popViewController(animated: true)
+    }
+    @objc func signIn(){
+        
+        if sView?.usernameTextField.text != "" && sView?.passwordTextField.text != "" {
+            let appdelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appdelegate.persistentContainer.viewContext
+            
+            let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+            
+            fetchRequest.predicate = NSPredicate(format: "username == %@", (sView?.usernameTextField.text)!)
+            
+            do{
+                let users = try context.fetch(fetchRequest)
+                
+                guard let user = users.first else {
+                    let alert = UIAlertController(title: "HATA", message: "Kullanıcı Bulunamadı", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "OK", style: .cancel)
+                    alert.addAction(action)
+                    self.present(alert,animated: true)
+                    return
+                }
+                if user.password == sView?.passwordTextField.text! {
+                    print("giris basarili")
+                }else {
+                    let alert = UIAlertController(title: "HATA", message: "Yanlış Şifre", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "OK", style: .cancel)
+                    alert.addAction(action)
+                    self.present(alert,animated: true)
+                }
+            }catch{
+                print("hata")
+            }
+        }else {
+            let alert = UIAlertController(title: "HATA", message: "Username ve Password Boş Olamaz!", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel)
+            alert.addAction(action)
+            self.present(alert,animated: true)
+        }
     }
 
 }
--- RegisterLesson/Features/Auth/SignUp/SignUpVC.swift
+++ RegisterLesson/Features/Auth/SignUp/SignUpVC.swift
@@ -1,11 +1,6 @@
-//
-//  SingUpVC.swift
-//  RegisterLesson
-//
-//  Created by macbook pro on 7.02.2024.
-//
 
 import UIKit
+import CoreData
 
 class SignUpVC: UIViewController {
 
@@ -16,11 +11,52 @@ class SignUpVC: UIViewController {
         view = sView
         navigationItem.hidesBackButton = true
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: sView!.backButton)
+        let gestureRec = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         
         sView?.backButton.addTarget(self, action: #selector(goPreviousPage), for: .touchUpInside)
+        sView?.signUpButton.addTarget(self, action: #selector(createAccount), for: .touchUpInside)
+    }
+    @objc func hideKeyboard() {
+        view.endEditing(true)
     }
     @objc func goPreviousPage(){
         navigationController?.popViewController(animated: true)
+    }
+    @objc func createAccount(){
+        
+        if sView?.usernameTextField.text != "" && sView?.passwordTextField.text != "" && sView?.confirmTextField.text != "" {
+            if sView?.passwordTextField.text == sView?.confirmTextField.text {
+                
+                let appdelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appdelegate.persistentContainer.viewContext
+                let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+                
+                user.setValue(sView?.usernameTextField.text, forKey: "username")
+                user.setValue(sView?.passwordTextField.text, forKey: "password")
+                user.setValue(UUID(), forKey: "id")
+                
+                do {
+                    try context.save()
+                    let alert = UIAlertController(title: "Başarılı", message: "Hesabınız başarıyla oluşturuldu", preferredStyle: .alert)
+                    let action = UIAlertAction(title: "Tamam", style: .cancel)
+                    alert.addAction(action)
+                    self.present(alert, animated: true)
+                }catch{
+                    print("error")
+                }
+                navigationController?.popViewController(animated: true)
+            }else {
+                let alert = UIAlertController(title: "HATA", message: "Şifreler Uyuşmuyor!", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .cancel)
+                alert.addAction(action)
+                self.present(alert, animated: true)
+            }
+        }else {
+            let alert = UIAlertController(title: "HATA", message: "Kullanıcı adı ve şifre boş olamaz!", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel)
+            alert.addAction(action)
+            self.present(alert, animated: true)
+        }
     }
 
 }
